0efc358fe976e3ec1ae7f0c48e7ce35aa5ff0a2c
TiagoLuis <111128607+TiagoLuis1@users.noreply.github.com>
2022-08-17 11:40:42 -0300

    Add files via upload

---
+++ Números entre 100 e 200/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace _15__Entre_100_e_200
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblN1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.lblResposta = new System.Windows.Forms.Label();
+            this.txtResposta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblN1
+            // 
+            this.lblN1.AutoSize = true;
+            this.lblN1.Location = new System.Drawing.Point(60, 27);
+            this.lblN1.Name = "lblN1";
+            this.lblN1.Size = new System.Drawing.Size(87, 13);
+            this.lblN1.TabIndex = 0;
+            this.lblN1.Text = "Insira um número";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(153, 20);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.Location = new System.Drawing.Point(153, 57);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(100, 62);
+            this.btnResultado.TabIndex = 2;
+            this.btnResultado.Text = "Descubra se está entre 100 e 200";
+            this.btnResultado.UseVisualStyleBackColor = true;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
+            // 
+            // lblResposta
+            // 
+            this.lblResposta.AutoSize = true;
+            this.lblResposta.Location = new System.Drawing.Point(65, 159);
+            this.lblResposta.Name = "lblResposta";
+            this.lblResposta.Size = new System.Drawing.Size(52, 13);
+            this.lblResposta.TabIndex = 3;
+            this.lblResposta.Text = "Resposta";
+            // 
+            // txtResposta
+            // 
+            this.txtResposta.Location = new System.Drawing.Point(153, 156);
+            this.txtResposta.Name = "txtResposta";
+            this.txtResposta.Size = new System.Drawing.Size(188, 20);
+            this.txtResposta.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResposta);
+            this.Controls.Add(this.lblResposta);
+            this.Controls.Add(this.btnResultado);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblN1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblN1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnResultado;
+        private System.Windows.Forms.Label lblResposta;
+        private System.Windows.Forms.TextBox txtResposta;
+    }
+}
+
---
+++ Números entre 100 e 200/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _15__Entre_100_e_200
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            double n1;
+
+            n1 = double.Parse(txtNumero.Text);
+
+            if (n1 > 100 && n1 < 200)
+            {
+                txtResposta.Text = "Este Número está entra 100 e 200";
+            }
+            else
+            {
+                txtResposta.Text = "Este número não está entre 100 e 200";
+            }
+        }
+    }
+}
