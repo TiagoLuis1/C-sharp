f1f91d0f885b735229fdd120166801b9a4b06efe
TiagoLuis <111128607+TiagoLuis1@users.noreply.github.com>
2022-08-17 11:38:34 -0300

    Add files via upload

---
+++ Média do aluno/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace _5__Média_aluno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblN1 = new System.Windows.Forms.Label();
+            this.lblN2 = new System.Windows.Forms.Label();
+            this.lblN3 = new System.Windows.Forms.Label();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.txtN3 = new System.Windows.Forms.TextBox();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblN1
+            // 
+            this.lblN1.AutoSize = true;
+            this.lblN1.Location = new System.Drawing.Point(77, 46);
+            this.lblN1.Name = "lblN1";
+            this.lblN1.Size = new System.Drawing.Size(42, 13);
+            this.lblN1.TabIndex = 0;
+            this.lblN1.Text = "Nota 1:";
+            // 
+            // lblN2
+            // 
+            this.lblN2.AutoSize = true;
+            this.lblN2.Location = new System.Drawing.Point(77, 84);
+            this.lblN2.Name = "lblN2";
+            this.lblN2.Size = new System.Drawing.Size(42, 13);
+            this.lblN2.TabIndex = 1;
+            this.lblN2.Text = "Nota 2:";
+            // 
+            // lblN3
+            // 
+            this.lblN3.AutoSize = true;
+            this.lblN3.Location = new System.Drawing.Point(77, 122);
+            this.lblN3.Name = "lblN3";
+            this.lblN3.Size = new System.Drawing.Size(42, 13);
+            this.lblN3.TabIndex = 2;
+            this.lblN3.Text = "Nota 3:";
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Location = new System.Drawing.Point(77, 219);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(39, 13);
+            this.lblMedia.TabIndex = 3;
+            this.lblMedia.Text = "Média:";
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.Location = new System.Drawing.Point(125, 158);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(100, 37);
+            this.btnResultado.TabIndex = 4;
+            this.btnResultado.Text = "Calcular a Média";
+            this.btnResultado.UseVisualStyleBackColor = true;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(125, 39);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 20);
+            this.txtN1.TabIndex = 5;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(125, 77);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 20);
+            this.txtN2.TabIndex = 6;
+            // 
+            // txtN3
+            // 
+            this.txtN3.Location = new System.Drawing.Point(125, 115);
+            this.txtN3.Name = "txtN3";
+            this.txtN3.Size = new System.Drawing.Size(100, 20);
+            this.txtN3.TabIndex = 7;
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Location = new System.Drawing.Point(125, 212);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.Size = new System.Drawing.Size(100, 20);
+            this.txtMedia.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.txtN3);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.btnResultado);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.lblN3);
+            this.Controls.Add(this.lblN2);
+            this.Controls.Add(this.lblN1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblN1;
+        private System.Windows.Forms.Label lblN2;
+        private System.Windows.Forms.Label lblN3;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Button btnResultado;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.TextBox txtN3;
+        private System.Windows.Forms.TextBox txtMedia;
+    }
+}
+
