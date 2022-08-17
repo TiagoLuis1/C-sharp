a47674db56d28e7f0c5b7652c47ebee971f259bc
TiagoLuis <111128607+TiagoLuis1@users.noreply.github.com>
2022-08-17 11:35:35 -0300

    Add files via upload

---
+++ Custo de um consumidor/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EXERCICIO012
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double custoFab;
+            double imp;
+            double distri;
+            double custoConsu;
+
+            custoFab = double.Parse(label1.Text);
+
+            
+            imp = (custoFab * 45) / 100;
+            
+            distri = (imp * 28) / 100;
+           
+            custoConsu = custoFab + distri + imp;
+
+            label2.Text = custoConsu.ToString();
+        }
+    }
+}
