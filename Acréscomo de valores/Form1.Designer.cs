5686086a6fb27888d13ce98a5aaf453cf2dde664
TiagoLuis <111128607+TiagoLuis1@users.noreply.github.com>
2022-08-17 11:33:49 -0300

    Add files via upload
    Exercício

---
+++ Acréscomo de valores/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace EXERCICIO11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCusto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPorcent = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtVendas = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Custo do produto:";
+            // 
+            // txtCusto
+            // 
+            this.txtCusto.Location = new System.Drawing.Point(195, 70);
+            this.txtCusto.Name = "txtCusto";
+            this.txtCusto.Size = new System.Drawing.Size(100, 20);
+            this.txtCusto.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor da porcentagem:";
+            // 
+            // txtPorcent
+            // 
+            this.txtPorcent.Location = new System.Drawing.Point(195, 109);
+            this.txtPorcent.Name = "txtPorcent";
+            this.txtPorcent.Size = new System.Drawing.Size(100, 20);
+            this.txtPorcent.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Valor Total";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtVendas
+            // 
+            this.txtVendas.Location = new System.Drawing.Point(195, 209);
+            this.txtVendas.Name = "txtVendas";
+            this.txtVendas.Size = new System.Drawing.Size(100, 20);
+            this.txtVendas.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Valor Total vai ser:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtVendas);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPorcent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCusto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCusto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPorcent;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtVendas;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
