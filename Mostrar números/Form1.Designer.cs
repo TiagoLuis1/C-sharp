6dea339ff9bf570f932f688e4abdfb68659e3976
TiagoLuis <111128607+TiagoLuis1@users.noreply.github.com>
2022-08-17 11:39:04 -0300

    Add files via upload

---
+++ Mostrar números/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace _18__idade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.txtIdade1 = new System.Windows.Forms.TextBox();
+            this.txtIdade2 = new System.Windows.Forms.TextBox();
+            this.txtIdade3 = new System.Windows.Forms.TextBox();
+            this.txtIdade4 = new System.Windows.Forms.TextBox();
+            this.txtIdade5 = new System.Windows.Forms.TextBox();
+            this.txtResp1 = new System.Windows.Forms.TextBox();
+            this.txtResp2 = new System.Windows.Forms.TextBox();
+            this.txtResp3 = new System.Windows.Forms.TextBox();
+            this.txtResp4 = new System.Windows.Forms.TextBox();
+            this.txtResp5 = new System.Windows.Forms.TextBox();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(20, 29);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(43, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Idade 1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(20, 68);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(43, 13);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Idade 2";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(20, 106);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(43, 13);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Idade 3";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(20, 145);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(43, 13);
+            this.lbl4.TabIndex = 3;
+            this.lbl4.Text = "Idade 4";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(20, 180);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(43, 13);
+            this.lbl5.TabIndex = 4;
+            this.lbl5.Text = "Idade 5";
+            // 
+            // txtIdade1
+            // 
+            this.txtIdade1.Location = new System.Drawing.Point(69, 22);
+            this.txtIdade1.Name = "txtIdade1";
+            this.txtIdade1.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade1.TabIndex = 5;
+            // 
+            // txtIdade2
+            // 
+            this.txtIdade2.Location = new System.Drawing.Point(69, 61);
+            this.txtIdade2.Name = "txtIdade2";
+            this.txtIdade2.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade2.TabIndex = 6;
+            // 
+            // txtIdade3
+            // 
+            this.txtIdade3.Location = new System.Drawing.Point(69, 99);
+            this.txtIdade3.Name = "txtIdade3";
+            this.txtIdade3.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade3.TabIndex = 7;
+            // 
+            // txtIdade4
+            // 
+            this.txtIdade4.Location = new System.Drawing.Point(69, 138);
+            this.txtIdade4.Name = "txtIdade4";
+            this.txtIdade4.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade4.TabIndex = 8;
+            // 
+            // txtIdade5
+            // 
+            this.txtIdade5.Location = new System.Drawing.Point(69, 173);
+            this.txtIdade5.Name = "txtIdade5";
+            this.txtIdade5.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade5.TabIndex = 9;
+            // 
+            // txtResp1
+            // 
+            this.txtResp1.Location = new System.Drawing.Point(192, 22);
+            this.txtResp1.Name = "txtResp1";
+            this.txtResp1.Size = new System.Drawing.Size(140, 20);
+            this.txtResp1.TabIndex = 10;
+            // 
+            // txtResp2
+            // 
+            this.txtResp2.Location = new System.Drawing.Point(192, 61);
+            this.txtResp2.Name = "txtResp2";
+            this.txtResp2.Size = new System.Drawing.Size(140, 20);
+            this.txtResp2.TabIndex = 11;
+            // 
+            // txtResp3
+            // 
+            this.txtResp3.Location = new System.Drawing.Point(192, 99);
+            this.txtResp3.Name = "txtResp3";
+            this.txtResp3.Size = new System.Drawing.Size(140, 20);
+            this.txtResp3.TabIndex = 12;
+            // 
+            // txtResp4
+            // 
+            this.txtResp4.Location = new System.Drawing.Point(192, 138);
+            this.txtResp4.Name = "txtResp4";
+            this.txtResp4.Size = new System.Drawing.Size(140, 20);
+            this.txtResp4.TabIndex = 13;
+            // 
+            // txtResp5
+            // 
+            this.txtResp5.Location = new System.Drawing.Point(192, 173);
+            this.txtResp5.Name = "txtResp5";
+            this.txtResp5.Size = new System.Drawing.Size(140, 20);
+            this.txtResp5.TabIndex = 14;
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.Location = new System.Drawing.Point(69, 213);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(263, 23);
+            this.btnResultado.TabIndex = 15;
+            this.btnResultado.Text = "Resultado";
+            this.btnResultado.UseVisualStyleBackColor = true;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnResultado);
+            this.Controls.Add(this.txtResp5);
+            this.Controls.Add(this.txtResp4);
+            this.Controls.Add(this.txtResp3);
+            this.Controls.Add(this.txtResp2);
+            this.Controls.Add(this.txtResp1);
+            this.Controls.Add(this.txtIdade5);
+            this.Controls.Add(this.txtIdade4);
+            this.Controls.Add(this.txtIdade3);
+            this.Controls.Add(this.txtIdade2);
+            this.Controls.Add(this.txtIdade1);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.TextBox txtIdade1;
+        private System.Windows.Forms.TextBox txtIdade2;
+        private System.Windows.Forms.TextBox txtIdade3;
+        private System.Windows.Forms.TextBox txtIdade4;
+        private System.Windows.Forms.TextBox txtIdade5;
+        private System.Windows.Forms.TextBox txtResp1;
+        private System.Windows.Forms.TextBox txtResp2;
+        private System.Windows.Forms.TextBox txtResp3;
+        private System.Windows.Forms.TextBox txtResp4;
+        private System.Windows.Forms.TextBox txtResp5;
+        private System.Windows.Forms.Button btnResultado;
+    }
+}
+
---
+++ Mostrar números/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _18__idade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            int n1;
+            int n2;
+            int n3;
+            int n4;
+            int n5;
+
+            n1 = int.Parse(txtIdade1.Text);
+            n2 = int.Parse(txtIdade2.Text);
+            n3 = int.Parse(txtIdade3.Text);
+            n4 = int.Parse(txtIdade4.Text);
+            n5 = int.Parse(txtIdade5.Text);
+
+            if (n1 >= 18)
+            {
+                txtResp1.Text = "Maior de idade";
+            }
+            else
+            {
+                txtResp1.Text = "Menor de idade";
+            }
+
+            if (n2 >= 18)
+            {
+                txtResp2.Text = "Maior de idade";
+            }
+            else
+            {
+                txtResp2.Text = "Menor de idade";
+            }
+
+            if (n3 >= 18)
+            {
+                txtResp3.Text = "Maior de idade";
+            }
+            else
+            {
+                txtResp3.Text = "Menor de idade";
+            }
+
+            if (n4 >= 18)
+            {
+                txtResp4.Text = "Maior de idade";
+            }
+            else
+            {
+                txtResp4.Text = "Menor de idade";
+            }
+
+            if (n5 >= 18)
+            {
+                txtResp5.Text = "Maior de idade";
+            }
+            else
+            {
+                txtResp5.Text = "Menor de idade";
+            }
+        }
+    }
+}
