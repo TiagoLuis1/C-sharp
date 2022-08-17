d9f0ab88bc88e456986d4404545b71ef26dead26
TiagoLuis <111128607+TiagoLuis1@users.noreply.github.com>
2022-08-17 11:42:33 -0300

    Add files via upload

---
+++ Salários/EXERCICIO04/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace EXERCICIO04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calcular_button_Click = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bimestre3TextBox = new System.Windows.Forms.TextBox();
+            this.bimestre2TextBox = new System.Windows.Forms.TextBox();
+            this.bimestre1TextBox = new System.Windows.Forms.TextBox();
+            this.materiaCombobox = new System.Windows.Forms.TextBox();
+            this.alunoTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calcular_button_Click
+            // 
+            this.calcular_button_Click.Location = new System.Drawing.Point(84, 232);
+            this.calcular_button_Click.Name = "calcular_button_Click";
+            this.calcular_button_Click.Size = new System.Drawing.Size(75, 23);
+            this.calcular_button_Click.TabIndex = 0;
+            this.calcular_button_Click.Text = "Calcular";
+            this.calcular_button_Click.UseVisualStyleBackColor = true;
+            this.calcular_button_Click.Click += new System.EventHandler(this.calcular_button_Click_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(203, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Lmpar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Media";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nota 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nota 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nota 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(84, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Matéria";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 206);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // bimestre3TextBox
+            // 
+            this.bimestre3TextBox.Location = new System.Drawing.Point(125, 174);
+            this.bimestre3TextBox.Name = "bimestre3TextBox";
+            this.bimestre3TextBox.Size = new System.Drawing.Size(100, 20);
+            this.bimestre3TextBox.TabIndex = 8;
+            // 
+            // bimestre2TextBox
+            // 
+            this.bimestre2TextBox.Location = new System.Drawing.Point(125, 142);
+            this.bimestre2TextBox.Name = "bimestre2TextBox";
+            this.bimestre2TextBox.Size = new System.Drawing.Size(100, 20);
+            this.bimestre2TextBox.TabIndex = 9;
+            // 
+            // bimestre1TextBox
+            // 
+            this.bimestre1TextBox.Location = new System.Drawing.Point(125, 111);
+            this.bimestre1TextBox.Name = "bimestre1TextBox";
+            this.bimestre1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.bimestre1TextBox.TabIndex = 10;
+            // 
+            // materiaCombobox
+            // 
+            this.materiaCombobox.Location = new System.Drawing.Point(125, 84);
+            this.materiaCombobox.Name = "materiaCombobox";
+            this.materiaCombobox.Size = new System.Drawing.Size(100, 20);
+            this.materiaCombobox.TabIndex = 11;
+            // 
+            // alunoTextBox
+            // 
+            this.alunoTextBox.Location = new System.Drawing.Point(125, 58);
+            this.alunoTextBox.Name = "alunoTextBox";
+            this.alunoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.alunoTextBox.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(84, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Aluno";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.alunoTextBox);
+            this.Controls.Add(this.materiaCombobox);
+            this.Controls.Add(this.bimestre1TextBox);
+            this.Controls.Add(this.bimestre2TextBox);
+            this.Controls.Add(this.bimestre3TextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.calcular_button_Click);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calcular_button_Click;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox bimestre3TextBox;
+        private System.Windows.Forms.TextBox bimestre2TextBox;
+        private System.Windows.Forms.TextBox bimestre1TextBox;
+        private System.Windows.Forms.TextBox materiaCombobox;
+        private System.Windows.Forms.TextBox alunoTextBox;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Salários/EXERCICIO04/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EXERCICIO04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcular_button_Click_Click(object sender, EventArgs e)
+        {
+            if (alunoTextBox.Text == "") ;
+            {
+                MessageBox.Show("O Campo nome é obrigatório", "Média", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            if (materiaCombobox.SelectedIndex == -1) ;
+            MessageBox.Show("Selecione uma matéria", "Média", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+            if (bimestre1TextBox.Text == "");
+
+            {
+            MessageBox.Show("Digite o valor do 1 bimestre", "Média", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+    {
+            if (alunoTextBox.Text == "") ;
+        }
+
+            MessageBox.Show("Digite o valor do 2 bimestre", "Média", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            
+
+}
+    }
+}
